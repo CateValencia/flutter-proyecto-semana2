f1f393b2607845329d5fa4616239ad653b3494f5
CateValencia <caterine.valencia@unillanos.edu.co>
2021-05-02 11:18:11 -0500

    fix ajuste funcional para return de calcular

--- lib/lib/pages/resultados.dart
+++ lib/lib/pages/resultados.dart
@@ -1,15 +1,69 @@
 import 'package:flutter/material.dart';
+import 'home-page.dart';
 
 class ResultadosPage extends StatelessWidget {
   
+ ResultadosPage();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Resultado de las operaciones"),
+        title: Text("Calculadora IMC"),
       ),
-      body: Column(),
+      body: _body(context),
     );
   }
+
+Widget _body(BuildContext context) {
+    
+    return Column(
+      children: [
+        Container(
+          alignment: Alignment.centerLeft,
+          padding: const EdgeInsets.fromLTRB(10, 20, 0, 20),
+          child: Text(
+            "Resultado",
+            style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+          ),
+        ),
+        
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => 
+                    HomePage(title: 'Calculadora IMC',)));
+          },
+          child: Container(
+              height: 80,
+              color: Colors.pink,
+              child: Column(children: [
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Text(
+                          "Calcular",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 22),
+                        ),
+                      ),
+                    ])
+              ])),
+        ),
+      ],
+    );
+  }
+
+
+
+
+
+
+
+
+
 }

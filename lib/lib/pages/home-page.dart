feb0ef5427301706bee838aee7a6dc493e20d8eb
CateValencia <caterine.valencia@unillanos.edu.co>
2021-05-01 21:54:58 -0500

    se agrega contenedor de Estatura

--- lib/lib/pages/home-page.dart
+++ lib/lib/pages/home-page.dart
@@ -13,7 +13,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
  
 
-  
+double _estatura = 166;  
 
   @override
   Widget build(BuildContext context) {
@@ -60,7 +60,67 @@ class _HomePageState extends State<HomePage> {
                                       height: 80),
                     ), Text("MUJER")
                   ])),
-            
+          Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Expanded(
+              child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF212122),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  height: 160,
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Text(
+                                "Estatura",
+                                style: TextStyle(
+                                    color: Colors.white30, fontSize: 20),
+                              )
+                            ]),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                _estatura.round().toString(),
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 50),
+                              ),
+                              Text("cm",
+                                  style: TextStyle(
+                                      color: Colors.white30, fontSize: 20))
+                            ]),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              SliderTheme(
+                                data: SliderThemeData(
+                                    activeTrackColor: Colors.white,
+                                    thumbColor: Colors.pink,
+                                    overlayColor: Colors.pink.withOpacity(0.2),
+                                    inactiveTrackColor: Colors.white24,
+                                    valueIndicatorColor: Colors.pink),
+                                child: Slider(
+                                  value: _estatura,
+                                  min: 100,
+                                  max: 200,
+                                  divisions: 100,
+                                  label:
+                                      _estatura.round().toString() +
+                                          " cm",
+                                  onChanged: (double value) {
+                                    setState(() {
+                                      _estatura = value;
+                                    });
+                                  },
+                                ),
+                              )
+                            ])
+                      ]))),
+            ),
           ],
         ),
       ],

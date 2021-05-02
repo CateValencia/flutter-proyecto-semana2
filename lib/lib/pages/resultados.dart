86e09cd6db786c4ac0226a7319b13971c9420e24
CateValencia <caterine.valencia@unillanos.edu.co>
2021-05-01 21:02:54 -0500

    creación de pages home&resultados

---
+++ lib/lib/pages/resultados.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ResultadosPage extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Resultado de las operaciones"),
+      ),
+      body: Column(),
+    );
+  }
+}

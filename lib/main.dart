e2ee92e3a4dc1533c45283086635bb96801c344d
CateValencia <caterine.valencia@unillanos.edu.co>
2021-05-02 21:15:29 -0500

    update title homepage

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Cálculo del IMC',
+      title: 'Calculadora IMC',
       theme: ThemeData(
         brightness: Brightness.dark,
         primaryColor: Colors.black,
